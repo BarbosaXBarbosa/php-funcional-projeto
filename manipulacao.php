1b757e22070ac48485088989b5abe5cb4211f499
BarbosaXBarbosa <asobrab123@hotmail.com>
2022-05-12 17:39:17 -0300

    Mapeando os dados.

--- manipulacao.php
+++ manipulacao.php
@@ -3,4 +3,13 @@
 $dados = require 'dados.php';
 
 $numeroPaises = count($dados);
-echo "Números de países: $numeroPaises\n"; 
+echo "Números de países: $numeroPaises\n";
+
+function convertePaisParaLetraMaiuscula(array $pais) {
+    $pais['pais'] = strtoupper($pais['pais']);
+    return $pais;
+}
+
+$dados = array_map('convertePaisParaLetraMaiuscula', $dados);
+
+var_dump($dados);
