5f30f037cf42ea0deca74f1a11a1b33c23ad4508
BarbosaXBarbosa <asobrab123@hotmail.com>
2022-05-13 11:12:24 -0300

    Ordenando com usort os paises.

--- contagem.php
+++ contagem.php
@@ -19,4 +19,18 @@ function medalhasAcumuladas(int $medalhasAcumuladas, array $pais): int
     return $medalhasAcumuladas + array_reduce($pais['medalhas'], 'somaMedalhas', 0);
 }
 
-echo array_reduce($dados, 'medalhasAcumuladas', 0);
+//echo array_reduce($dados, 'medalhasAcumuladas', 0);
+
+function ordenaPais($item1, $item2) {
+    $medalhas1 = $item1['medalhas'];
+    $medalhas2 = $item2['medalhas'];
+
+    return $medalhas2['ouro'] - $medalhas1['ouro'] !== 0
+        ? $medalhas2['ouro'] - $medalhas1['ouro']
+        : ($medalhas2['prata'] - $medalhas1['prata'] !== 0
+            ? $medalhas2['prata'] - $medalhas1['prata'] 
+            : $medalhas2['bronze'] - $medalhas1['bronze']);
+};
+usort($dados, 'ordenaPais');
+
+var_dump($dados);
